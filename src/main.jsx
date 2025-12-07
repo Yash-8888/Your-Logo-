ae272f6d92a49a00eaaaa76d3d1f05dc5e231b1f
Yash-8888 <codingwithcandy@gmail.com>
2025-12-07 17:51:34 +0530

    UI Added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Home from "./components/Home/Home.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import About from "./components/About/About.jsx";
+import Contact from "./components/Contact-us/Contact-us.jsx";
+Contact
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/about", element: <About /> },
+      {path: "/Contact-us", element: <Contact /> }
+      
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
