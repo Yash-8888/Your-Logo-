c726273f30b02116413a237d2eba5ae846e63751
Yash-8888 <codingwithcandy@gmail.com>
2025-12-08 17:06:32 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,23 +3,34 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Home from "./components/Home/Home.jsx";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import About from "./components/About/About.jsx";
 import Contact from "./components/Contact-us/Contact-us.jsx";
 Contact
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/about", element: <About /> },
-      {path: "/Contact-us", element: <Contact /> }
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: [
+//       { path: "/", element: <Home /> },
+//       { path: "/about", element: <About /> },
+//       {path: "/Contact-us", element: <Contact /> }
       
-    ],
-  },
-]);
+//     ],
+//   },
+// ]);]
+
+// method 2
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <route path="/" element={<App />}>
+      <route path="" element={<Home/>} />
+      <route path="" element={<About/>} />
+      <route path="" element={<Contact-us/>} />
+    </route>
+  )
+)
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
